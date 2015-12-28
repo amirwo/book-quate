ebc2c9b5fd7fcc9e9295ed6423d8542fee0aacf7
amirwo <amir.volfovich@startapp.com>
2015-12-28 11:30:46 +0200

    added alot of stuff

--- src/com/gama/quatenation/model/RequestListener.java
+++
@@ -1,7 +0,0 @@
-package com.gama.quatenation.model;
-
-public interface RequestListener {
-
-	public void onRequestComplete();
-	public void onRequestError();
-}
--- src/com/gama/quatenation/service/RequestListener.java
+++
@@ -1,7 +0,0 @@
-package com.gama.quatenation.service;
-
-public interface RequestListener {
-
-	public <T> void onRequestComplete(T response);
-	public void onRequestError(int errCode);
-}
